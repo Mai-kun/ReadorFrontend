52179ce5315e70be9caaf46b71983439c6c79ab0
4svFox <googleavto@rambler.ru>
2025-05-19 19:50:27 +0300

    Исправил отступы в форме авторизации (?)

--- src/pages/ModerationPage.jsx
+++ src/pages/ModerationPage.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { moderationApi } from '../api/auth';
 import '../styles/ModerationPage.css';
 
