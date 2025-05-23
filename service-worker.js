33a72d9d5fa6b0a42353e5e39ca1929b7a4bc305
4svFox <googleavto@rambler.ru>
2025-05-23 15:04:02 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -10,49 +10,8 @@ self.addEventListener('install', (event) => {
                 '/',
                 '/index.html',
                 '/static/manifest.json',
-                '/static/js/bundle.js',
                 OFFLINE_PAGE,
                 //'/placeholder-cover.jpg'
             ]))
     );
-});
-
-self.addEventListener('fetch', (event) => {
-    const url = new URL(event.request.url);
-
-    // Кэшируем API запросы к контенту книг
-    if (url.pathname.endsWith('/text')) {
-        event.respondWith(
-            caches.match(event.request)
-                .then(cached => cached || fetch(event.request)
-                ));
-        return;
-    }
-
-    if (event.request.mode === 'navigate') {
-        event.respondWith(
-            fetch(event.request)
-                .catch(() => caches.match(OFFLINE_PAGE))
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request)
-                .then(response => response || fetch(event.request))
-        );
-    }
-});
-
-self.addEventListener('message', (event) => {
-    if (event.data.action === 'CACHE_BOOK') {
-        const { url, content } = event.data.payload;
-        event.waitUntil(
-            caches.open(DYNAMIC_CACHE)
-                .then(cache => {
-                    const response = new Response(JSON.stringify(content), {
-                        headers: { 'Content-Type': 'application/json' }
-                    });
-                    return cache.put(url, response);
-                })
-        );
-    }
 });
